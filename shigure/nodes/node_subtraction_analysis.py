908ab7ec0236932d32ebef3a8128842dd58aa646
Yukiho-YOSHIEDA <is0436er@ed.ritsumei.ac.jp>
2020-12-31 19:10:32 +0900

    [Update] Image Message を Compressed Image Message に変更

--- shigure/nodes/node_subtraction_analysis.py
+++ shigure/nodes/node_subtraction_analysis.py
@@ -1,7 +1,7 @@
 import rclpy
 import cv2
 import numpy as np
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 
 from shigure.nodes.node_image_preview import ImagePreviewNode
@@ -17,23 +17,23 @@ class SubtractionAnalysisNode(ImagePreviewNode):
         self.bridge = CvBridge()
         self.subtraction_frames = SubtractionFrames()
         self.bg_subtraction_logic = SubtractionAnalysisLogic()
-        self._publisher = self.create_publisher(Image,
+        self._publisher = self.create_publisher(CompressedImage,
                                                 '/shigure/subtraction_analysis', 10)
-        self._subscription = self.create_subscription(Image, '/shigure/bg_subtraction',
+        self._subscription = self.create_subscription(CompressedImage, '/shigure/bg_subtraction',
                                                       self.get_subtraction_callback, 10)
 
-    def get_subtraction_callback(self, image_rect_raw: Image):
+    def get_subtraction_callback(self, image_rect_raw: CompressedImage):
         try:
             # FPS計算
             self.frame_count_up()
 
-            frame: np.ndarray = self.bridge.imgmsg_to_cv2(image_rect_raw)
+            frame: np.ndarray = self.bridge.compressed_imgmsg_to_cv2(image_rect_raw)
             result, data = self.bg_subtraction_logic.execute(self.subtraction_frames)
             self.subtraction_frames.add_frame(frame, Timestamp(image_rect_raw.header.stamp.sec,
                                                                image_rect_raw.header.stamp.nanosec))
 
             if result:
-                msg: Image = self.bridge.cv2_to_imgmsg(data)
+                msg: Image = self.bridge.cv2_to_compressed_imgmsg(data)
                 sec, nano_sec = self.subtraction_frames.get_timestamp()
                 msg.header.stamp.sec = sec
                 msg.header.stamp.nanosec = nano_sec
@@ -43,11 +43,6 @@ class SubtractionAnalysisNode(ImagePreviewNode):
                     img = self.print_fps(data)
                     cv2.imshow("Result", cv2.hconcat([cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR), img]))
                     cv2.waitKey(1)
-
-
-                #
-                # # 結果の表示
-                # cv2.imshow("color_src", color_img)
         except Exception as err:
             self.get_logger().error(err)
 
