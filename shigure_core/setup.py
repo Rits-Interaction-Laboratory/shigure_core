c926f240eba8cc42b24e872d57537acf0fe3e55a
koukemo <java.sparrow22.surface@gmail.com>
2022-05-27 16:03:25 +0900

    [feat] launchファイルの追加

--- shigure_core/setup.py
+++ shigure_core/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import setup
 
 package_name = 'shigure_core'
@@ -19,6 +22,7 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
