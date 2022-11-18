f56a524b1d884a727106002e45f6295fbe2a64d6
koukemo <java.sparrow22.surface@gmail.com>
2022-11-18 13:59:59 +0900

    [fix] poseテーブル内のカラム名称を修正

--- shigure_core/resource/db/migration/7_create_pose_table.up.sql
+++ shigure_core/resource/db/migration/7_create_pose_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `pose` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `savedata` INT NOT NULL,
-  `save_id` INT NOT NULL, 
+  `sequence_id` INT NOT NULL,
+  `frame_number` INT NOT NULL, 
   `pose_key_points_list` JSON NOT NULL,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`));
