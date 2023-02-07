5fedd1f1a947cf540247a3072e064001a6f777ee
aarimoto <67216471+arinnko5@users.noreply.github.com>
2023-02-07 13:06:12 +0900

    Create frame_object_item.py

---
+++ shigure_core/shigure_core/nodes/yolox_object_detection/frame_object_item.py
@@ -0,0 +1,51 @@
+from typing import Tuple
+
+import numpy as np
+
+from shigure_core.enum.detected_object_action_enum import DetectedObjectActionEnum
+from shigure_core.nodes.common_model.timestamp import Timestamp
+from shigure_core.nodes.common_model.bounding_box import BoundingBox
+
+
+class FrameObjectItem:
+
+    def __init__(self, action: DetectedObjectActionEnum, bounding_box: BoundingBox, size: int,
+                 mask: np.ndarray, detected_at: Timestamp):
+        self._action = action
+        self._bounding_box = bounding_box
+        self._size = size
+        self._mask = mask
+        self._detected_at = detected_at
+
+    def is_match(self, other) -> Tuple[bool, int]:
+        if self.action != other.action:
+            return False, 0
+
+        left: BoundingBox = self.bounding_box
+        right: BoundingBox = other.bounding_box
+
+        return left.is_collided(right)
+
+    @property
+    def items(self) -> Tuple[DetectedObjectActionEnum, BoundingBox, int, np.ndarray, Timestamp]:
+        return self._action, self._bounding_box, self._size, self._mask, self._detected_at
+
+    @property
+    def action(self) -> DetectedObjectActionEnum:
+        return self._action
+
+    @property
+    def bounding_box(self) -> BoundingBox:
+        return self._bounding_box
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def mask(self) -> np.ndarray:
+        return self._mask
+
+    @property
+    def detected_at(self) -> Timestamp:
+        return self._detected_at
