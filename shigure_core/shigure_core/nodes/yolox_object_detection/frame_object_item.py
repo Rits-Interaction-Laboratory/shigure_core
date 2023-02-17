8d8135ebe7c046e6bd10511370208b2a7d27cd7d
aarimoto <67216471+arinnko5@users.noreply.github.com>
2023-02-17 11:57:12 +0900

    Update frame_object_item.py

--- shigure_core/shigure_core/nodes/yolox_object_detection/frame_object_item.py
+++ shigure_core/shigure_core/nodes/yolox_object_detection/frame_object_item.py
@@ -10,7 +10,7 @@ from shigure_core.nodes.common_model.bounding_box import BoundingBox
 class FrameObjectItem:
 
     def __init__(self, action: DetectedObjectActionEnum, bounding_box: BoundingBox, size: int,
-                 mask: np.ndarray, detected_at: Timestamp, class_id:str):
+                 mask: np.ndarray, detected_at: Timestamp, class_id: str):
         self._action = action
         self._bounding_box = bounding_box
         self._size = size
@@ -29,7 +29,7 @@ class FrameObjectItem:
 
     @property
     def items(self) -> Tuple[DetectedObjectActionEnum, BoundingBox, int, np.ndarray, Timestamp]:
-        return self._action, self._bounding_box, self._size, self._mask, self._detected_at
+        return self._action, self._bounding_box, self._size, self._mask, self._detected_at,self._class_id
 
     @property
     def action(self) -> DetectedObjectActionEnum:
@@ -50,3 +50,4 @@ class FrameObjectItem:
     @property
     def detected_at(self) -> Timestamp:
         return self._detected_at
+        
